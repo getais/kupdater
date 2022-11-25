8276e0415f73fce9d3d0c26b5a29cc132510afe2
Tomas Liumparas <tomas@bestview.lt>
2022-11-25 02:52:01 +0200

    Deployment annotation changes trigger AppVersion changes

--- controllers/deployment_controller.go
+++ controllers/deployment_controller.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"strings"
 	"time"
 
@@ -62,41 +63,55 @@ func (r *DeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		return ctrl.Result{}, err
 	}
 
-	// log.Info("Reconciling")
-	appver := &opsv1alpha1.AppVersion{}
-	err = r.Get(ctx, types.NamespacedName{Name: dep.Name, Namespace: dep.Namespace}, appver)
+	// Check if object should be processed
+	err = r.FilterDeployment(dep)
+	if err != nil {
+		return ctrl.Result{}, nil
+	}
+
+	// Lookup AppVersion instance
+	appverFound := &opsv1alpha1.AppVersion{}
+	appverNew, err := r.NewAppver(dep)
+	if err != nil {
+		log.Error(err, "Invalid Deployment annotations")
+		return ctrl.Result{RequeueAfter: 2 * time.Minute}, err
+	}
+
+	err = r.Get(ctx, types.NamespacedName{Name: dep.Name, Namespace: dep.Namespace}, appverFound)
+
+	// Handle AppVersion creation
 	if err != nil && errors.IsNotFound(err) {
 
-		// If Pod has annotations
-		if _, enabled := dep.Annotations["kupdater.ops.getais.cloud/enabled"]; enabled {
+		// If Source repo annotation is present
+		if _, source := dep.Annotations["kupdater.ops.getais.cloud/source"]; source {
 
-			// If Source repo annotation is present
-			if _, source := dep.Annotations["kupdater.ops.getais.cloud/source"]; source {
-
-				// Create AppVersion to track updates on
-				appversions, err := r.NewAppver(dep)
-				if err != nil {
-					log.Error(err, "Invalid Deployment")
-					return ctrl.Result{RequeueAfter: 2 * time.Minute}, err
-				}
-
-				for _, appver := range appversions {
-					// Create Appversion{
-					log.Info("Creating a new AppVersion")
-					err = r.Create(ctx, appver)
-					if err != nil {
-						log.Error(err, "Failed to create new AppVersion")
-						return ctrl.Result{RequeueAfter: 2 * time.Minute}, err
-					}
-				}
+			// Create new AppVersion instance
+			log.Info("Creating a new AppVersion")
+			err = r.Create(ctx, appverNew)
+			if err != nil {
+				log.Error(err, "Failed to create new AppVersion")
+				return ctrl.Result{RequeueAfter: 2 * time.Minute}, err
 			}
-
 		}
+
 		// AppVersion created successfully - return and requeue
 		return ctrl.Result{}, nil
 	} else if err != nil {
 		log.Error(err, "Failed to get Application")
 		return ctrl.Result{}, err
+	}
+
+	// Handle AppVersion updates
+	if !reflect.DeepEqual(appverFound.Spec, appverNew.Spec) {
+		log.Info("Updating AppVersion instance")
+
+		appverFound.Spec = appverNew.Spec
+
+		err = r.Update(ctx, appverFound)
+		if err != nil {
+			log.Error(err, "Failed to update AppVersion instance")
+			return ctrl.Result{RequeueAfter: 2 * time.Minute}, nil
+		}
 	}
 
 	log.Info("Reconciled")
@@ -111,20 +126,20 @@ func (r *DeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func (r *DeploymentReconciler) NewAppver(a *appsv1.Deployment) (AppVersions []*opsv1alpha1.AppVersion, err error) {
+func (r *DeploymentReconciler) NewAppver(a *appsv1.Deployment) (AppVersion *opsv1alpha1.AppVersion, err error) {
 
 	Source, success := a.Annotations["kupdater.ops.getais.cloud/source"]
 	if !success {
-		return []*opsv1alpha1.AppVersion{}, fmt.Errorf("Missing AppVersion source")
+		return &opsv1alpha1.AppVersion{}, fmt.Errorf("missing AppVersion source")
 	}
 
 	Type, success := a.Annotations["kupdater.ops.getais.cloud/type"]
 	if !success {
-		return []*opsv1alpha1.AppVersion{}, fmt.Errorf("Missing AppVersion type")
+		return &opsv1alpha1.AppVersion{}, fmt.Errorf("missing AppVersion type")
 	}
 
 	var Version string
-	Version, _ = a.Annotations["kupdater.ops.getais.cloud/version"]
+	Version = a.Annotations["kupdater.ops.getais.cloud/version"]
 
 	for _, Container := range a.Spec.Template.Spec.Containers {
 		AppVer := new(opsv1alpha1.AppVersion)
@@ -133,7 +148,7 @@ func (r *DeploymentReconciler) NewAppver(a *appsv1.Deployment) (AppVersions []*o
 			Version = strings.Split(Container.Image, ":")[1]
 		}
 
-		AppVer = &opsv1alpha1.AppVersion{
+		AppVersion = &opsv1alpha1.AppVersion{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      a.Name,
 				Namespace: a.Namespace,
@@ -148,8 +163,16 @@ func (r *DeploymentReconciler) NewAppver(a *appsv1.Deployment) (AppVersions []*o
 		}
 		// Set Application instance as the owner and controller
 		ctrl.SetControllerReference(a, AppVer, r.Scheme)
-		AppVersions = append(AppVersions, AppVer)
 	}
-	return AppVersions, nil
+	return AppVersion, nil
 
+}
+
+func (r *DeploymentReconciler) FilterDeployment(dep *appsv1.Deployment) error {
+
+	_, success := dep.Annotations["kupdater.ops.getais.cloud/enabled"]
+	if !success {
+		return fmt.Errorf("missing kupdater.ops.getais.cloud/enabled annotation")
+	}
+	return nil
 }
